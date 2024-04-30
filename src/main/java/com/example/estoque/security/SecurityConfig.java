3bb0fa19ae2ffa42f033e1ab12a669d7c27b3054
Joao Paulo <jpl71330@gmail.com>
2024-04-30 18:11:39 -0300

    Consertando o 'ERROR 403'

--- src/main/java/com/example/estoque/security/SecurityConfig.java
+++ src/main/java/com/example/estoque/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.estoque.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,6 +20,7 @@ public class SecurityConfig {
                 authorize.requestMatchers("/logout").permitAll();
                 authorize.anyRequest().authenticated();
             })
+            .formLogin(Customizer.withDefaults())
             .build();
     }
 }
