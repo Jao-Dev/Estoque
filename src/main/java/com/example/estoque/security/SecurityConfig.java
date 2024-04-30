c58ef140ed1edbd9a23c58c6e4cd72d04e75304d
Joao Paulo <jpl71330@gmail.com>
2024-04-30 18:10:47 -0300

    implementando o Spring Security

---
+++ src/main/java/com/example/estoque/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.estoque.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(authorize -> {
+                authorize.requestMatchers("/estoque").permitAll();
+                authorize.requestMatchers("/logout").permitAll();
+                authorize.anyRequest().authenticated();
+            })
+            .build();
+    }
+}
